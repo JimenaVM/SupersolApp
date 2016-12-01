e831cca18311acb4a9ad55eb052001129457f028
Gonzalo Condori Villca <gonzalocondorivillca@gmail.com>
2016-12-01 00:39:07 -0400

    imagen de logo

---
+++ app/src/main/java/com/gonzalo/supersolapp/PerfilUsuarioActivity.java
@@ -0,0 +1,22 @@
+package com.gonzalo.supersolapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gonzalopro on 12/1/16.
+ */
+
+public class PerfilUsuarioActivity extends AppCompatActivity {
+
+    private String idUsuario;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+
+
+    }
+}
